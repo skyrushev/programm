a42184e325f455501e127e2faaadc4eccf5a5ca8
Sergey <Skyrushev@rambler.ru>
2016-09-16 22:04:48 +0300

    add

---
+++ ПодсчётСлов/SimpleTextEditor/Form1.Designer.cs
@@ -0,0 +1,287 @@
+﻿namespace SimpleTextEditor
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.open = new System.Windows.Forms.Button();
+            this.listBox2 = new System.Windows.Forms.ListBox();
+            this.listBox3 = new System.Windows.Forms.ListBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.listBox4 = new System.Windows.Forms.ListBox();
+            this.listBox5 = new System.Windows.Forms.ListBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(434, 77);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(169, 25);
+            this.textBox2.TabIndex = 1;
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 16;
+            this.listBox1.Location = new System.Drawing.Point(12, 249);
+            this.listBox1.MultiColumn = true;
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(122, 132);
+            this.listBox1.TabIndex = 2;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 34);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.textBox1.Size = new System.Drawing.Size(412, 180);
+            this.textBox1.TabIndex = 1;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(628, 79);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "считать";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Visible = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // open
+            // 
+            this.open.Location = new System.Drawing.Point(675, 54);
+            this.open.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.open.Name = "open";
+            this.open.Size = new System.Drawing.Size(67, 23);
+            this.open.TabIndex = 1;
+            this.open.Text = "Open";
+            this.open.UseVisualStyleBackColor = true;
+            this.open.Click += new System.EventHandler(this.open_Click);
+            // 
+            // listBox2
+            // 
+            this.listBox2.FormattingEnabled = true;
+            this.listBox2.ItemHeight = 16;
+            this.listBox2.Location = new System.Drawing.Point(140, 249);
+            this.listBox2.Name = "listBox2";
+            this.listBox2.Size = new System.Drawing.Size(120, 132);
+            this.listBox2.TabIndex = 3;
+            // 
+            // listBox3
+            // 
+            this.listBox3.FormattingEnabled = true;
+            this.listBox3.ItemHeight = 16;
+            this.listBox3.Location = new System.Drawing.Point(266, 249);
+            this.listBox3.Name = "listBox3";
+            this.listBox3.Size = new System.Drawing.Size(135, 20);
+            this.listBox3.TabIndex = 4;
+            // 
+            // label1
+            // 
+            this.label1.Location = new System.Drawing.Point(430, 11);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(189, 63);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Количество слов в тексте (с помощью регулярных выражений )";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 216);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(46, 17);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "слова";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(137, 216);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(64, 17);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "повторы";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(263, 216);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(177, 17);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "количество слов в тексте";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(15, 10);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(46, 17);
+            this.label5.TabIndex = 9;
+            this.label5.Text = "Текст";
+            // 
+            // listBox4
+            // 
+            this.listBox4.FormattingEnabled = true;
+            this.listBox4.ItemHeight = 16;
+            this.listBox4.Location = new System.Drawing.Point(434, 125);
+            this.listBox4.Name = "listBox4";
+            this.listBox4.Size = new System.Drawing.Size(176, 260);
+            this.listBox4.TabIndex = 10;
+            // 
+            // listBox5
+            // 
+            this.listBox5.FormattingEnabled = true;
+            this.listBox5.ItemHeight = 16;
+            this.listBox5.Location = new System.Drawing.Point(615, 145);
+            this.listBox5.Name = "listBox5";
+            this.listBox5.Size = new System.Drawing.Size(164, 20);
+            this.listBox5.TabIndex = 11;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(434, 105);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(176, 17);
+            this.label6.TabIndex = 12;
+            this.label6.Text = "не повтаряющиеся слова";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(612, 105);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(158, 17);
+            this.label7.TabIndex = 13;
+            this.label7.Text = "количество таких слов";
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Location = new System.Drawing.Point(266, 345);
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(143, 22);
+            this.numericUpDown1.TabIndex = 14;
+            // 
+            // label8
+            // 
+            this.label8.Location = new System.Drawing.Point(266, 272);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(163, 57);
+            this.label8.TabIndex = 15;
+            this.label8.Text = "искать повторы с количеством букв более";
+            // 
+            // label9
+            // 
+            this.label9.Location = new System.Drawing.Point(653, 9);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(117, 43);
+            this.label9.TabIndex = 16;
+            this.label9.Text = "Файлы в TXT. формате";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(791, 397);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.numericUpDown1);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.listBox5);
+            this.Controls.Add(this.listBox4);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.listBox3);
+            this.Controls.Add(this.listBox2);
+            this.Controls.Add(this.open);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.Name = "Form1";
+            this.Text = "Simple Text Editor";
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button open;
+        private System.Windows.Forms.ListBox listBox2;
+        private System.Windows.Forms.ListBox listBox3;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.ListBox listBox4;
+        private System.Windows.Forms.ListBox listBox5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+    }
+}
---
+++ ПодсчётСлов/SimpleTextEditor/Form1.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Text.RegularExpressions;
+
+namespace SimpleTextEditor
+{
+    using System.IO;
+
+    public partial class Form1 : Form
+    {
+        string name;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void open_Click(object sender, EventArgs e)
+        {
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                name = openFileDialog1.FileName;
+                textBox1.Clear();
+                textBox1.Text = File.ReadAllText(name, Encoding.Default);
+                button1.Visible = true;
+                listBox1.Items.Clear();
+                listBox2.Items.Clear();
+
+            }
+        }
+  
+     /*   private void save_Click(object sender, EventArgs e)
+        {
+            if (saveFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                string name = saveFileDialog1.FileName;
+                File.WriteAllText(name, textBox1.Text);
+            }
+        }*/ 
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            listBox1.Items.Clear();
+            listBox2.Items.Clear();
+            listBox3.Items.Clear();
+            listBox4.Items.Clear();
+            listBox5.Items.Clear();
+            
+         //  Регулярные выражения
+            string sentence = File.ReadAllText(name, Encoding.Default);
+            string слова = @"\b(\w|[-])+\b";
+            int a = Regex.Matches(sentence, слова).Count;
+            string b = Convert.ToString(a);
+            textBox2.Text= b ;
+
+            int словоДлина = (int)numericUpDown1.Value;
+        
+            string[] words = sentence.Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+            var result = words.GroupBy(x => x)
+                .Where(x => x.Count() > 1 && x.Key.Length > словоДлина)
+                .Select(x => new { Word = x.Key, Frequency = x.Count(), Number = words.Count() });
+            foreach (var item in result)
+                listBox1.Items.Add(item.Word);
+         
+            string[] words1 = sentence.Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+            var result1 = words.GroupBy(x => x)
+                .Where(x => x.Count() > 1 && x.Key.Length > словоДлина)
+                .Select(x => new { Word = x.Key, Frequency = x.Count(), Number = words.Count() });
+            foreach (var item in result1)
+                listBox2.Items.Add(item.Frequency);
+        
+            string[] words2 = sentence.Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+            var result2 = words.GroupBy(x => x)
+                .Where(x => x.Count() > 1 && x.Key.Length > словоДлина)
+                .Select(x => new { Word = x.Key, Frequency = x.Count(), Number = words.Count() });
+            foreach (var item in result2)
+                listBox3.Items.Add(item.Number);
+            
+            string[] words3 = sentence.Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+           var result3 = words3.GroupBy(x => x) 
+             .Where(x => x.Count() < 2)
+             
+              .Select(x => new { Unik = x.Key });
+         //  int A = result1.Count() ;
+           foreach (var item in result3)
+               listBox4.Items.Add(item.Unik);
+
+           string[] words4 = sentence.Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+           var result4 = words4.GroupBy(x => x)
+             .Where(x => x.Count() < 2);
+
+            //  .Select(x => new { Unik = x.Key });
+           int A = result4.Count();
+           foreach (var item in result4)
+               listBox5.Items.Add(A);
+
+            /*    Console.WriteLine("Слово: {0}\tКоличество повторов: {1} \t Количество слов {2} ", item.Word,
+                    item.Frequency, item.Number);*/
+            
+        // Уверен что код можно оптимизировать, но я пока не знаю как.    
+        }
+    }
+}
